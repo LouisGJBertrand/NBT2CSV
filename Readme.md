4981bf8a79118d6a26f45ab3074e45daef555fb2
Louis 'Loé' BERTRAND <louisgjbertrand@icloud.com>
2024-08-27 20:01:16 +0200

    Feat: NBT Tool

---
+++ Readme.md
@@ -0,0 +1,70 @@
+# NBT 2 CSV Tool for Create Schematics and structure blocks
+
+This tool exports in csv format every blocks required for structure blocks, create schematics etc.
+
+/!\ some blocks like grass are placed in the schematicanon via dirt blocks, this tool will not register grassblocks as dirt but as grass blocks
+
+This tool uses the NBT standard file format. it is compatible with every GZIPed nbt files (structures, create schematics etc.)
+
+# Usage
+
+put all your .nbt files you wish to convert to csv in the input folder, then run the script with
+
+```bash
+py .\nbt_util.py
+```
+
+the script will output every csv to the output folder as well as a global one with cumulated block count.
+
+The python file has a `__main__` test hence you can import it in your project as a standard python module.
+
+
+Getting the CSV of the block count for the structure
+
+```py
+# exportCSV :
+#   True    = will return the total block dictionary and export the file to CSV
+#   False   = will return the total block dictionary
+Block_needed = NBT2CSV(filename = "GlobalPathTo\\YourFile.nbt", exportCSV = True)
+```
+
+Combining two block dictionaries
+
+```py
+
+```
+
+
+# Requirements
+
+```
+Python 3.+
+pip
+NBT Module (https://pypi.org/project/NBT/)
+```
+
+# Installation
+
+click on code then download the zip of the git. Unzip in a folder.
+
+alternatively you can clone the git with
+
+```
+git clone https://github.com/LouisGJBertrand/NBT2CSV.git
+```
+
+to install the nbt module execute the following command:
+
+```batch
+pip install NBT
+```
+
+# License
+
+```
+Tool:
+MIT License Standard - (C) Louis BERTRAND
+
+Initial method for reading NBT Data
+https://stackoverflow.com/questions/74857267/how-do-i-convert-a-string-nbt-into-json-with-python
+```
---
+++ nbt_util.py
@@ -0,0 +1,107 @@
+import os
+from nbt import nbt # type: ignore
+
+def CombineElements(PaletteA: dict, PaletteB: dict):
+
+    for key in PaletteB:
+        block_name = key
+        if PaletteA.__contains__(block_name):
+            PaletteA[block_name] += PaletteB[block_name]
+            continue
+        PaletteA[block_name] = PaletteB[block_name]
+
+    return PaletteA
+
+def NBT2CSV(filename, exportCSV = True):
+
+    nbtfile = nbt.NBTFile(filename,'rb')
+
+    
+    palette_data = nbtfile["palette"]
+    blocks = nbtfile["blocks"]
+
+
+    palette = []
+    i = 0
+    for palette_el in palette_data:
+        el_name = str(palette_el["Name"])
+
+        palette.append(el_name)
+        i+=1
+        continue
+
+    block_dict = {}
+    for blocks_list_element in blocks:
+        
+        # the id has to be converted to integer
+        palette_element_id = int(str(blocks_list_element["state"]))
+        palette_block_name = palette[palette_element_id]
+
+        if(block_dict.__contains__(palette_block_name)):
+            block_dict[palette_block_name] += 1
+            continue
+        block_dict[palette_block_name] = 1
+
+
+    if exportCSV:
+        csv = "block_name,count\n"
+
+        for key in block_dict:
+            block_name = key
+            csv += "\""+block_name+"\","+str(block_dict[block_name])+"\n"
+
+        f = open(filename.replace("input", "output").replace(".nbt", ".csv"), "w")
+        f.write(csv)
+        f.close()
+
+    return block_dict
+
+def main():
+    
+    print("NBT TO CSV UTIL")
+    print("Author Louis 'Loé' BERTRAND <github@louisgjbertrand>")
+    print("Made For GoodSoup <3")
+    print("version 1.0\n\n")
+
+    input_directory = os.listdir("./input")
+    GlobalBlockCount = {}
+
+    file_counter = 0
+    for file_name in input_directory:
+
+        print ("found ./input/" + file_name)
+        file_counter+=1
+
+    print("\ntotal files to convert: "+ str(file_counter)+"\n")
+
+    file_counter = 0
+    while file_counter < len(input_directory):
+
+        file_name = str(input_directory[file_counter])
+
+        print ("converting ./input/" + file_name)
+        filename = "./input/" + file_name
+
+        block_dict = NBT2CSV(filename, True)
+        GlobalBlockCount = CombineElements( GlobalBlockCount , block_dict )
+
+        print ("saved at ./output/" + file_name.replace(".nbt", ".csv"))
+        file_counter+=1
+
+    print("\nConvertion finished ("+ str(file_counter)+")")
+
+    csv = "block_name,count\n"
+
+    for key in GlobalBlockCount:
+        csv += "\""+key+"\","+str(GlobalBlockCount[key])+"\n"
+
+
+    f = open("./output/global.csv", "a")
+    f.write(csv)
+    f.close()
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
